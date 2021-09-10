6109f1751c2ccba4c6ba10b4c1d7fd61377828d3
rokixy <zzubillzzu@126.com>
2021-09-11 02:09:50 +0800

    deploy

---
+++ precache-manifest.76e00f34234948b05f652a3e2d335ba0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "277a2cdb7a02e0d09767",
+    "url": "/keep-booking/css/app.6bc527ac.css"
+  },
+  {
+    "revision": "95d45eb4fc8edc8f658520b0fb01ca39",
+    "url": "/keep-booking/index.html"
+  },
+  {
+    "revision": "277a2cdb7a02e0d09767",
+    "url": "/keep-booking/js/app.313a650f.js"
+  },
+  {
+    "revision": "9b239b484c7d95fe7681",
+    "url": "/keep-booking/js/chunk-vendors.194cb391.js"
+  },
+  {
+    "revision": "34fa58f264e125e51ff260d77309bdbf",
+    "url": "/keep-booking/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/keep-booking/robots.txt"
+  }
+]);
